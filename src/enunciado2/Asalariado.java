285fd55bbf6cbb048aa1e14126a3589525cbbbb2
Dani Clavel <7dcv2005@gmail.com>
2024-02-15 14:01:52 +0100

    Avance del ejercicio 2

--- src/enunciado2/Asalariado.java
+++ src/enunciado2/Asalariado.java
@@ -1,5 +1,47 @@
 package enunciado2;
 
 public class Asalariado {
+	// Creamos variables
+	String nombre;
+	long dni;
+	int diasVacaciones;
+	
+	// Creamos constructores
+	public Asalariado() {
+		nombre = "";
+		dni = 0;
+		diasVacaciones = 0;
+	}
+	
+	public Asalariado(String nombre, long dni, int diasVacaciones) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.diasVacaciones = diasVacaciones;
+	}
 
+	// Generamos los Getters and Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public long getDni() {
+		return dni;
+	}
+
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
+
+	public int getDiasVacaciones() {
+		return diasVacaciones;
+	}
+
+	public void setDiasVacaciones(int diasVacaciones) {
+		this.diasVacaciones = diasVacaciones;
+	}
+		
 }
---
+++ src/enunciado2/EmpleadoProduccion.java
@@ -0,0 +1,6 @@
+package enunciado2;
+
+//Extends de Asalariado
+public class EmpleadoProduccion extends Asalariado {
+
+}
